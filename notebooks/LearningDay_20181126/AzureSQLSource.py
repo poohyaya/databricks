8e68899f12a994b28ba072e72850a0fb65b7f4e8
poohyaya <32959357+poohyaya@users.noreply.github.com>
2018-11-19 13:38:26 +0900

    Update: README.md notebooks

--- notebooks/LearningDay_20181126/AzureSQLSource.py
+++ notebooks/LearningDay_20181126/AzureSQLSource.py
@@ -17,6 +17,25 @@ connectionProperties = {
 
 # Push down query
 # pushdown query need as result set alias
-pushdown_query = "(SELECT * FROM SalesLT.Product) product"
+pushdown_query = "(SELECT * FROM SalesLT.Product WITH (NOLOCK)) product"
 df = spark.read.jdbc(url=jdbcUrl, table=pushdown_query, properties=connectionProperties)
 display(df)
+
+# COMMAND ----------
+
+# CREATE TABLE using JDBC Driver
+%sql
+CREATE TABLE IF NOT EXISTS jdbcProduct
+USING org.apache.spark.sql.jdbc
+OPTIONS (
+  url "jdbc:sqlserver://kothkimpaasdb1.database.windows.net:1433;databasename=AdventureWorksLT",
+  dbtable "[SalesLT].[Product]",
+  user "pfesql",
+  password "Password1!@#"
+)
+
+# COMMAND ----------
+
+# Show CREATE TABLE statement
+%sql
+SHOW CREATE TABLE jdbcProduct
